e457c4bcc3d74fc296cbc896c34be68092ba41cc
Aedrian Ponce <aedrianponce1203@gmail.com>
2026-09-22 03:05:33 +0800

    feat: add lightweight Ani PCM audio worklet

---
+++ public/ani/pcm-capture-worklet.js
@@ -0,0 +1,12 @@
+class AniPcmCaptureProcessor extends AudioWorkletProcessor {
+  process(inputs) {
+    const channel = inputs[0]?.[0];
+    if (channel?.length) {
+      const copy = new Float32Array(channel);
+      this.port.postMessage(copy.buffer, [copy.buffer]);
+    }
+    return true;
+  }
+}
+
+registerProcessor('ani-pcm-capture', AniPcmCaptureProcessor);
